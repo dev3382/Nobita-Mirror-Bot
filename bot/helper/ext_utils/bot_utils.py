d6eb35c9c48acbe7c8cf23b11319c0e64d375e4d
Theodore-xw4886 <89044682+Theodore-xw4886@users.noreply.github.com>
2021-08-17 12:50:56 +0600

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -97,7 +97,7 @@ def get_progress_bar_string(status):
     p = min(max(p, 0), 100)
     cFull = p // 8
     cPart = p % 8 - 1
-    p_str = '🟨' * cFull
+    p_str = '⬜' * cFull
     if cPart >= 0:
         p_str += PROGRESS_INCOMPLETE[cPart]
     p_str += '🟥' * (PROGRESS_MAX_SIZE - cFull)
